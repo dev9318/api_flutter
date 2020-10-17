97f99ceb3af58e553e383efe72cc0ea6be28d885
dev9318 <dev.moxaj@gmail.com>
2020-10-17 19:19:45 +0530

    updated main

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,23 @@ void main() {
   runApp(MyApp());
 }
 
+class Data {
+  final int userId;
+  final int id;
+  final String title;
+
+
+  Data({this.userId, this.id, this.title});
+
+  factory Data.fromJson(Map<String, dynamic> json) {
+    return Data(
+      userId: json['userId'] as int,
+      id: json['id'] as int,
+      title: json['title'] as String,
+    );
+  }
+}
+
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
@@ -31,14 +48,19 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  String txt = '';
+  String search = '';
+  List<Data> list = [];
 
   void _getData() async{
-    String url = 'https://jsonplaceholder.typicode.com/todos/1';
+    String url = 'https://jsonplaceholder.typicode.com/albums';
+    print(1);
     Response response = await get(url);
-    Map data = jsonDecode(response.body);
+    print(2);
+    final parsed = jsonDecode(response.body).cast<Map<String, dynamic>>();
+     list = parsed.map<Data>((json) => Data.fromJson(json)).toList();
+
     setState(() {
-      txt = data['title'];
+      list = list;
     });
   }
   void _putData() async{
@@ -48,10 +70,10 @@ class _MyHomePageState extends State<MyHomePage> {
       'Content-Type': 'application/json; charset=UTF-8',
       },
       body: jsonEncode(<String, String>{
-        'title': "Bar-foo",
+        'title': search,
       }));
     setState(() {
-      txt = response.body;
+      print(response);
     });
   }
 
@@ -62,7 +84,42 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: Center(
+      body: ListView.builder(
+        itemBuilder: (context, index) {
+          if (index == 0){
+            return Padding(
+              padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
+              child: TextFormField(
+                  autofocus: false,
+                  decoration: const InputDecoration(
+                    hintText: 'Type your search',
+                    hintStyle: TextStyle(color: Colors.grey),
+                    filled: true,
+                    fillColor: Colors.white70,
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(12.0)),
+                      borderSide: BorderSide(color: Colors.grey, width: 2),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                      borderSide: BorderSide(color: Colors.black),
+                    ),
+                    labelText: 'search',
+                  ),
+                  validator: (val) => val.isEmpty ? 'Enter your search' : null,
+                  onChanged: (val) {
+                    setState(() => search = val.trim());
+                  }
+              ),
+            );
+          }
+          return Card(
+            child: Text(list[index].title),
+          );
+        },
+        itemCount: 1+list.length,
+      ),
+      /*body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
@@ -75,7 +132,7 @@ class _MyHomePageState extends State<MyHomePage> {
             ),
           ],
         ),
-      ),
+      )*/
       floatingActionButton: FloatingActionButton(
         onPressed: _getData,
         tooltip: 'Load data',
